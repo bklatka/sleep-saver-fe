22060f50e54ea2e5260db21039b383d51e0343ad
Bernard Klatka <bklatka@evolution.com>
2024-12-11 20:06:20 +0100

    fix: remove plugin for refresz from babelrc

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { ThemeProvider } from '@mui/material';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 import App from './App';
-
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
