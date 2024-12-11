e1f223a84f517409d404711abee4236ac3d1092f
Bernard Klatka <bklatka@evolution.com>
2024-12-11 20:45:14 +0100

    fix: remove port from prod url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const config = {
-  apiUrl: process.env.REACT_APP_API_URL || process.env.NODE_ENV === "production" ? 'https://sleep-saver-be.onrender.com:4000' : 'http://localhost:4000',
+  apiUrl: process.env.REACT_APP_API_URL || process.env.NODE_ENV === "production" ? 'https://sleep-saver-be.onrender.com' : 'http://localhost:4000',
 };
