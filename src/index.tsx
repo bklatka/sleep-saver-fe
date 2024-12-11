fd6417b24254e7e84b41e08a0c88249b3fd74902
Bernard Klatka <bklatka@evolution.com>
2024-12-11 20:57:54 +0100

    feat: add PWA

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,19 @@ root.render(
   </React.StrictMode>
 );
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful');
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
+
 if (process.env.NODE_ENV === 'development') {
-  // Enable hot module replacement
   if (module.hot) {
     module.hot.accept();
   }
